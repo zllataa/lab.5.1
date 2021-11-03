8ea6c992744ad67d0b390cb1153d6c8ab4a08e45
Zlata Polovka <91230475+zllataa@users.noreply.github.com>
2021-11-04 02:41:48 +0300

    Add files via upload

---
+++ lab.5.1/lab.5.1/lab.5.1.cpp
@@ -0,0 +1,25 @@
+﻿// Lab 5_1
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double h(const double x, const double y); // прототип
+
+int main()
+{
+	double s, t;
+
+
+	cout << "s = "; cin >> s;
+	cout << "t = "; cin >> t;
+	double k = (h(s * s, t * t) + h(s + t, 1) * h(s + t, 1)) / (1 + h(s * t, 2) * h(s * t, 2));
+
+	cout << "k = " << k << endl;
+
+	return 0;
+}
+double h(const double x, const double y) // визначення
+{
+	return (x * y) / (1 + x * x * y * y);
+}
