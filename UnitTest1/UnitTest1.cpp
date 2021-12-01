f9982f49ab027ffedd7e456c4048e73da35118fc
Zlata Polovka <91230475+zllataa@users.noreply.github.com>
2021-12-01 20:40:06 +0300

    Add files via upload

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab.5.1/lab.5.1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double t;
+			t = h(-1, 2);
+			Assert::AreEqual(t, -0.4);
+		}
+	};
+}
